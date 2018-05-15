113a2fd861231ac8416e08a7592862e639458a04
Dhara Shah <DharaShah@Dharas-MacBook-Air.local>
2018-05-15 19:07:44 +0530

    added image counter to check if images are loaded

--- src/js/Container.jsx
+++ src/js/Container.jsx
@@ -11,7 +11,8 @@ export default class toCreditsCard extends React.Component {
         card_data: {},
         configs: {}
       },
-      optionalConfigJSON: {}
+      optionalConfigJSON: {},
+      image_count: 1
     };
 
     if (this.props.dataJSON) {
@@ -29,7 +30,6 @@ export default class toCreditsCard extends React.Component {
   }
 
   componentDidMount() {
-    console.log("did mount")
     if (this.state.fetchingData){
       let items_to_fetch = [
         axios.get(this.props.dataURL)
@@ -49,100 +49,122 @@ export default class toCreditsCard extends React.Component {
     }
   }
 
-  componentDidUpdate(){
-    let items = document.querySelectorAll('.credits-links'),
-      scroll_area = document.querySelector('.scroll-area'),
-      length = items.length,
-      width = 0;
-    if (scroll_area) {
-      for(let i = 0; i < length; i++) {
-        width += (items[i].getBoundingClientRect().width + (this.state.renderMode === "col4" ? 10 : 5));
-      }
-      scroll_area.style.width = `${width}px`;
-    }
-    if (scroll_area) {
-      var window_items = [],
-        min = 0,
-        max = items.length - 1,
-        navBar = document.querySelector('.credits-logos'),
-        navBarBBox = navBar.getBoundingClientRect(),
-        stateOfNavbar = [];
-      for (let i = 0; i < max; i++) {
-        let left = items[i].getBoundingClientRect().left,
-          width = items[i].getBoundingClientRect().width;
+  setWidth(){
+    console.log(this.state.image_count, this.state.dataJSON.card_data.data.section.length)
+    if (this.state.image_count === this.state.dataJSON.card_data.data.section.length){
+      let items = document.querySelectorAll('.credits-links'),
+        scroll_area = document.querySelector('.scroll-area'),
+        section = document.querySelectorAll('.credits-section'),
+        section_length = section.length,
+        length = items.length,
+        width = 0;
 
-        if ((left + width) <= navBarBBox.width) {
-          window_items.push(i);
+      if (scroll_area) {
+        for(let i = 0; i < length; i++) {
+          width += (items[i].getBoundingClientRect().width + (this.state.renderMode === "col4" ? 10 : 5));
         }
+        scroll_area.style.width = `${width + section_length * 30}px`;
       }
 
-      stateOfNavbar.push({
-        window_items: window_items,
-        scrollLeft: 0
-      });
+      if (scroll_area){
+        let window_items = [],
+          min = 0,
+          max = items.length - 1,
+          navBar = document.querySelector('.credits-logos'),
+          stateOfNavbar = [],
+          navBarBBox = navBar.getBoundingClientRect();
 
-      document.querySelector('#prev-arrow').addEventListener('click', (e) => {
-        let popedElement = stateOfNavbar.pop(),
-          currentElement = stateOfNavbar[stateOfNavbar.length - 1],
-          next = document.querySelector('#next-arrow');
+        console.log(max, "max")
 
-        window_items = currentElement.window_items;
+        for (let i = 0; i < max; i++) {
+          let left = items[i].getBoundingClientRect().left,
+            width = items[i].getBoundingClientRect().width;
 
-        if (next.style.display !== 'inline-block') {
-          next.style.display = 'inline-block';
+          if ((left + width) <= navBarBBox.width) {
+            window_items.push(i);
+          }
         }
 
-        document.querySelector('.credits-logos').style.overflow = 'scroll';
-        document.querySelector('.credits-logos').scrollLeft = currentElement.scrollLeft;
-        document.querySelector('.credits-logos').style.overflow = 'hidden';
+        stateOfNavbar.push({
+          window_items: window_items,
+          scrollLeft: 0
+        });
 
-        if (stateOfNavbar.length === 1) {
-          document.querySelector('#prev-arrow').style.display = 'none';
-        }
-      });
+        console.log(stateOfNavbar, "stateOfNavbar")
+        document.querySelector('#prev-arrow').addEventListener('click', (e) => {
+          console.log("hey prev", stateOfNavbar)
+          let popedElement = stateOfNavbar.pop(),
+            currentElement = stateOfNavbar[stateOfNavbar.length - 1],
+            next = document.querySelector('#next-arrow');
 
-      document.querySelector('#next-arrow').addEventListener('click', (e) => {
-        let firstElement = window_items[0],
-          lastElement = window_items[window_items.length - 1],
-          new_width = 0,
-          new_window_items = [],
-          prev = document.querySelector('#prev-arrow');
+          console.log(currentElement,"currentElement" )
+          
+          window_items = currentElement.window_items;
 
-        if (lastElement !== max) {
-          if (prev.style.display !== 'inline-block') {
-            prev.style.display = 'inline-block';
+          if (next.style.display !== 'inline-block') {
+            next.style.display = 'inline-block';
           }
-
-          for (let i = firstElement + 1; i <= max; i++) {
-            let element = document.querySelector(`.credits-links[data-item="${i}"]`),
-              width = element.getBoundingClientRect().width;
-
-            if ((new_width + width) <= navBarBBox.width) {
-              new_width += width;
-              new_window_items.push(i);
-            } else {
-              break;
-            }
-          }
-          window_items = new_window_items.sort((a, b) => a - b);
-
-          let nextElem = document.querySelector(`.credits-links[data-item="${window_items[0]}"]`),
-            scrollLeft = document.querySelector('.credits-logos').scrollLeft,
-            newScrollLeft = scrollLeft + nextElem.getBoundingClientRect().left;
-
-          stateOfNavbar.push({
-            window_items: window_items,
-            scrollLeft: newScrollLeft
-          });
 
           document.querySelector('.credits-logos').style.overflow = 'scroll';
-          document.querySelector('.credits-logos').scrollLeft = newScrollLeft;
+          document.querySelector('.credits-logos').scrollLeft = currentElement.scrollLeft;
           document.querySelector('.credits-logos').style.overflow = 'hidden';
 
-          if (window_items[window_items.length - 1] === max) {
-            document.querySelector('#next-arrow').style.display = 'none';
+          if (stateOfNavbar.length === 1) {
+            document.querySelector('#prev-arrow').style.display = 'none';
           }
-        }
+        });
+
+        document.querySelector('#next-arrow').addEventListener('click', (e) => {
+          let firstElement = window_items[0],
+            lastElement = window_items[window_items.length - 1],
+            new_width = 0,
+            new_window_items = [],
+            prev = document.querySelector('#prev-arrow');
+
+          if (lastElement !== max) {
+            if (prev.style.display !== 'inline-block') {
+              prev.style.display = 'inline-block';
+            }
+
+            for (let i = firstElement; i < max; i++) {
+              console.log(document.querySelector(`.credits-links[data-item="${i}"]`))
+              let element = document.querySelector(`.credits-links[data-item="${i}"]`),
+                width = element.getBoundingClientRect().width;
+
+              if ((new_width + width) <= navBarBBox.width) {
+                new_width += width;
+                new_window_items.push(i);
+              } else {
+                break;
+              }
+            }
+            window_items = new_window_items.sort((a, b) => a - b);
+
+            let nextElem = document.querySelector(`.credits-links[data-item="${window_items[0]}"]`),
+              scrollLeft = document.querySelector('.credits-logos').scrollLeft,
+              newScrollLeft = scrollLeft + nextElem.getBoundingClientRect().left;
+
+            stateOfNavbar.push({
+              window_items: window_items,
+              scrollLeft: newScrollLeft
+            });
+
+            document.querySelector('.credits-logos').style.overflow = 'scroll';
+            document.querySelector('.credits-logos').scrollLeft = newScrollLeft;
+            document.querySelector('.credits-logos').style.overflow = 'hidden';
+
+            if (window_items[window_items.length - 1] === max) {
+              document.querySelector('#next-arrow').style.display = 'none';
+            }
+          }
+        });
+      }
+
+    } else {
+      this.setState((prevState, props) => {
+        return {
+          image_count: prevState.image_count + 1
+        };
       });
     }
   }
@@ -201,10 +223,10 @@ export default class toCreditsCard extends React.Component {
       for (let key in grouped_data){
         let names = grouped_data[key].map((d,i) =>{
           return(
-            <div className="credits-links" data-item={i + 1} style={{display: 'inline-block'}}>
+            <div className="credits-links" data-item={i} style={{display: 'inline-block'}}>
               <a href={d.url} target="_blank">
                 <div className="company-name">{d.name}</div>
-                <div className="company-logo"><img src={d.logo}/></div>
+                <div className="company-logo"><img src={d.logo} onLoad={this.setWidth.bind(this)}/></div>
               </a>
             </div>
           )
@@ -273,7 +295,7 @@ export default class toCreditsCard extends React.Component {
       )
     }
   }
-  
+
   render() {    
     switch(this.props.mode) {
       case 'col16':
